ae974841a4ad6a38a596fd9fffd4eb73b2bb7fbd
Argos <kiron656@gmail.com>
2025-09-23 12:57:19 -0500

    Primer commit - servidor WebSocket

---
+++ server.js
@@ -0,0 +1,19 @@
+const WebSocket = require("ws");
+
+// Render asigna el puerto en process.env.PORT
+const PORT = process.env.PORT || 3000;
+const server = new WebSocket.Server({ port: PORT });
+
+server.on("connection", (socket) => {
+  console.log("Cliente conectado");
+  socket.send("Bienvenido al WebSocket!");
+
+  socket.on("message", (msg) => {
+    console.log("Mensaje recibido:", msg.toString());
+    socket.send(`Eco: ${msg}`);
+  });
+
+  socket.on("close", () => console.log("Cliente desconectado"));
+});
+
+console.log(`Servidor WebSocket escuchando en puerto ${PORT}`);
